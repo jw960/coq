f61a09f5dd496be4c1b245b683016695981c523c
Emilio Jesus Gallego Arias <e+git@x80.org>
2018-10-24 00:58:22 +0200

    [plugins] Import change-analytics plugin.
    From https://github.com/uwplse/coq-change-analytics

---
+++ plugins/analytics/analytical_lib.ml
@@ -0,0 +1,24 @@
+(* --- Command infrastructure --- *)
+
+(*
+ * Various hooks to register once we can
+ *)
+let print_state_add (v : Vernacexpr.vernac_control CAst.t) (state : Stateid.t) : unit =
+  Feedback.msg_notice
+    (Pp.str (Printf.sprintf "ADD: %s\n" (Stateid.to_string state)))
+
+let print_state_edit (state : Stateid.t) : unit =
+  Feedback.msg_notice
+    (Pp.str (Printf.sprintf "EDIT: %s\n" (Stateid.to_string state)))
+
+let print_state_exec (state : Stateid.t) : unit =
+  Feedback.msg_notice
+    (Pp.str (Printf.sprintf "EXEC: %s\n" (Stateid.to_string state)))
+
+let hooks : Stm.document_edit_notifiers =
+  Stm.{ add_hook  = print_state_add
+      ; edit_hook = print_state_edit
+      ; exec_hook = print_state_exec
+      }
+
+let _ = Hook.set Stm.document_edit_hook hooks
