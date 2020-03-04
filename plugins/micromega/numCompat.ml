173d27535f4141563b3c1d6680ee3add88bea0f6
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-03-04 18:43:24 -0500

    [micromega] [zarith] [debug] Comparison of module Z, zarith vs num

--- plugins/micromega/numCompat.ml
+++ plugins/micromega/numCompat.ml
@@ -30,7 +30,41 @@ module type ZArith = sig
   val to_string : t -> string
 end
 
-module Z = struct
+module Z_old = struct
+  type t = Big_int.big_int
+
+  open Big_int
+
+  let zero = zero_big_int
+  let one = unit_big_int
+  let two = big_int_of_int 2
+  let add = Big_int.add_big_int
+  let sub = Big_int.sub_big_int
+  let mul = Big_int.mult_big_int
+  let div = Big_int.div_big_int
+  let neg = Big_int.minus_big_int
+  let sign = Big_int.sign_big_int
+  let equal = eq_big_int
+  let compare = compare_big_int
+  let power_int = power_big_int_positive_int
+  let quomod = quomod_big_int
+
+  let ppcm x y =
+    let g = gcd_big_int x y in
+    let x' = div_big_int x g in
+    let y' = div_big_int y g in
+    mult_big_int g (mult_big_int x' y')
+
+  let gcd = gcd_big_int
+
+  let lcm x y =
+    if eq_big_int x zero && eq_big_int y zero then zero
+    else abs_big_int (div_big_int (mult_big_int x y) (gcd x y))
+
+  let to_string = string_of_big_int
+end
+
+module Z_new = struct
   include Z
 
   (* Workaround https://github.com/ocaml/Zarith/issues/58 , remove
@@ -107,9 +141,8 @@ module type QArith = sig
   val to_float : t -> float
 end
 
-module Q : QArith with module Z = Z = struct
-
-  module Z = Z
+module Q_new : QArith with module Z = Z_new = struct
+  module Z = Z_new
 
   let pow_check_exp x y =
     let z_res =
@@ -159,7 +192,6 @@ module Q : QArith with module Z = Z = struct
 
   (* XXX: review / improve *)
   (* Note that Z.pow doesn't support negative exponents *)
-
   let pow2 y = pow_check_exp Z.two y
   let pow10 y = pow_check_exp Z.ten y
 
@@ -173,3 +205,195 @@ module Q : QArith with module Z = Z = struct
     in
     pow_check_exp (Z.of_int x) y
 end
+
+module Q_old : QArith with module Z = Z_old = struct
+  module Z = Z_old
+
+  type t = Num.num
+
+  open Num
+
+  let of_int x = Int x
+  let zero = Int 0
+  let one = Int 1
+  let two = Int 2
+  let ten = Int 10
+  let neg_one = Int (-1)
+
+  module Notations = struct
+    let ( // ) = div_num
+    let ( +/ ) = add_num
+    let ( -/ ) = sub_num
+    let ( */ ) = mult_num
+    let ( =/ ) = eq_num
+    let ( <>/ ) = ( <>/ )
+    let ( >/ ) = ( >/ )
+    let ( >=/ ) = ( >=/ )
+    let ( </ ) = ( </ )
+    let ( <=/ ) = ( <=/ )
+  end
+
+  let compare = compare_num
+  let make x y = Big_int x // Big_int y
+
+  let numdom r =
+    let r' = Ratio.normalize_ratio (ratio_of_num r) in
+    (Ratio.numerator_ratio r', Ratio.denominator_ratio r')
+
+  let num x = numdom x |> fst
+  let den x = numdom x |> snd
+  let of_bigint x = Big_int x
+  let to_bigint = big_int_of_num
+  let neg = minus_num
+
+  (* let inv =  *)
+  let max = max_num
+  let min = min_num
+  let sign = sign_num
+  let abs = abs_num
+  let mod_ = mod_num
+  let floor = floor_num
+  let ceiling = ceiling_num
+  let round = round_num
+  let pow2 n = power_num two (Int n)
+  let pow10 n = power_num ten (Int n)
+  let power x = power_num (Int x)
+  let to_string = string_of_num
+  let of_string = num_of_string
+  let to_float = float_of_num
+end
+
+(* XXX *)
+let _ = Q_new.zero
+let _ = Q_old.zero
+
+module Z_mix : ZArith with type t = Z_old.t * Z_new.t = struct
+  module Zo = Z_old
+  module Zn = Z_new
+
+  type t = Zo.t * Zn.t
+
+  let eq_ name (x : Zo.t) (y : Zn.t) : unit =
+    let x_ = Zo.to_string x in
+    let y_ = Zn.to_string y in
+    let a = String.equal x_ y_ in
+    if a then ()
+    else
+      let () = Format.eprintf "assertion failure at %s, [%s] vs [%s]@\n%!" name x_ y_ in
+      assert false
+
+  let eqg name x y : unit =
+    let a = x = y in
+    if a then ()
+    else
+      let () = Format.eprintf "assertion failure at %s@\n%!" name in
+      assert false
+
+  let lift1 name fo fn x =
+    let y_old = fo (fst x) in
+    let y_new = fn (snd x) in
+    eq_ name y_old y_new; (y_old, y_new)
+
+  let lift1b name fo fn x =
+    let y_old = fo (fst x) in
+    let y_new = fn (snd x) in
+    eqg name y_old y_new; y_old
+
+  let lift2b name fo fn x y =
+    let r_old = fo (fst x) (fst y) in
+    let r_new = fn (snd x) (snd y) in
+    eqg name r_old r_new; r_old
+
+  let lift2 name fo fn x y =
+    let r_old = fo (fst x) (fst y) in
+    let r_new = fn (snd x) (snd y) in
+    eq_ name r_old r_new; (r_old, r_new)
+
+  let lift22 name fo fn x y =
+    let r_old1, r_old2 = fo (fst x) (fst y) in
+    let r_new1, r_new2 = fn (snd x) (snd y) in
+    eq_ name r_old1 r_new1;
+    eq_ name r_old2 r_new2;
+    ((r_old1, r_new1), (r_old2, r_new2))
+
+  let zero = (Z_old.zero, Z_new.zero)
+  let one = (Z_old.one, Z_new.one)
+  let two = (Z_old.two, Z_new.two)
+  let add = lift2 "add" Zo.add Zn.add
+  let sub = lift2 "sub" Zo.sub Zn.sub
+  let mul = lift2 "mul" Zo.mul Zn.mul
+  let div = lift2 "div" Zo.div Zn.div
+  let neg = lift1 "new" Zo.neg Zn.neg
+  let sign = lift1b "sign" Zo.sign Zn.sign
+  let equal = lift2b "eq" Zo.equal Zn.equal
+  let compare = lift2b "compare" Zo.compare Zn.compare
+  let power_int x i = lift2 "power_int" Zo.power_int Zn.power_int x (i, i)
+  let quomod : t -> t -> t * t = lift22 "quomod" Zo.quomod Zn.quomod
+  let ppcm = lift2 "ppcm" Zo.ppcm Zn.ppcm
+  let gcd = lift2 "gcd" Zo.gcd Zn.gcd
+  let lcm = lift2 "lcm" Zo.lcm Zn.lcm
+  let to_string = lift1b "to_string" Zo.to_string Zn.to_string
+end
+
+module Q_old_z_new : QArith with module Z = Z_mix = struct
+  module Z = Z_mix
+
+  type t = Num.num
+
+  open Num
+
+  let of_int x = Int x
+  let zero = Int 0
+  let one = Int 1
+  let two = Int 2
+  let ten = Int 10
+  let neg_one = Int (-1)
+
+  module Notations = struct
+    let ( // ) = div_num
+    let ( +/ ) = add_num
+    let ( -/ ) = sub_num
+    let ( */ ) = mult_num
+    let ( =/ ) = eq_num
+    let ( <>/ ) = ( <>/ )
+    let ( >/ ) = ( >/ )
+    let ( >=/ ) = ( >=/ )
+    let ( </ ) = ( </ )
+    let ( <=/ ) = ( <=/ )
+  end
+
+  let compare = compare_num
+  let make x y = Big_int (fst x) // Big_int (fst y)
+
+  let numdom r =
+    let r' = Ratio.normalize_ratio (ratio_of_num r) in
+    (Ratio.numerator_ratio r', Ratio.denominator_ratio r')
+
+  let conv x = Z_new.of_string (Z_old.to_string x)
+  let num x = numdom x |> fst |> fun x -> (x, conv x)
+  let den x = numdom x |> snd |> fun x -> (x, conv x)
+  let of_bigint x = Big_int (fst x)
+  let to_bigint x = (big_int_of_num x, conv (big_int_of_num x))
+  let neg = minus_num
+
+  (* let inv =  *)
+  let max = max_num
+  let min = min_num
+  let sign = sign_num
+  let abs = abs_num
+  let mod_ = mod_num
+  let floor = floor_num
+  let ceiling = ceiling_num
+  let round = round_num
+  let pow2 n = power_num two (Int n)
+  let pow10 n = power_num ten (Int n)
+  let power x = power_num (Int x)
+  let to_string = string_of_num
+  let of_string = num_of_string
+  let to_float = float_of_num
+end
+
+let _ = Q_old_z_new.zero
+
+module Z = Z_mix
+module Q = Q_old_z_new
