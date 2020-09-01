c4bb0353d74775dd86d287677a99378d3b35195a
Emilio Jesus Gallego Arias <e+git@x80.org>
2020-09-02 01:53:02 +0200

    Use simple compiler as coqc
    Still a few tweaks to be done:
    - -quiet / etc..
    - errors

--- compiler/scoqc.ml
+++ compiler/scoqc.ml
@@ -12,9 +12,10 @@ let mk_vo_path ?(has_ml=false) unix_path coq_path implicit =
   let coq_path = Libnames.dirpath_of_string coq_path in
   { Loadpath.unix_path; coq_path; has_ml; implicit; recursive = true }
 
-let default_vo_load_path =
-  [ mk_vo_path (Filename.concat Coq_config.coqlib "theories") "Coq" true
-  ; mk_vo_path (Filename.concat Coq_config.coqlib "plugins")  "Coq" true ~has_ml:true
+let default_vo_load_path ~coqlib =
+  let coqlib = Option.default Coq_config.coqlib coqlib in
+  [ mk_vo_path (Filename.concat coqlib "theories") "Coq" true
+  ; mk_vo_path (Filename.concat coqlib "plugins")  "Coq" true ~has_ml:true
   ]
 
 let dirpath_of_file f =
@@ -29,9 +30,13 @@ let dirpath_of_file f =
   let ldir = Libnames.add_dirpath_suffix ldir0 id in
   ldir
 
-let fb_handler = function
+let fb_handler = let open Feedback in function
   | Feedback.{ contents; _ } ->
     match contents with
+    | Feedback.Message(Debug,_,_)
+    | Feedback.Message(Info,_,_)
+    | Feedback.Message(Notice,_,_) ->
+      ()
     | Feedback.Message(_lvl,_loc,msg)->
       Format.printf "%s@\n%!" Pp.(string_of_ppcmds msg)
     | _ -> ()
@@ -44,11 +49,13 @@ let load_objs libs =
   in
   List.(iter rq_file (rev libs))
 
-let require_libs = ["Coq.Init.Prelude", None, Some false]
+let dft_require_libs = ["Coq.Init.Prelude", None, Some false]
 
-let init_coq ~vo_path ~ml_path in_file =
+let init_coq ~vo_path ~ml_path ~require_libs in_file =
   Lib.init ();
   (* Global.set_engagement Declarations.PredicativeSet; *)
+  (* Global.set_engagement Declarations.PredicativeSet; *)
+  Global.set_indices_matter false;
   Flags.set_native_compiler false;
   Global.set_native_compiler false;
   Safe_typing.allow_delayed_constants := false;
@@ -78,42 +85,73 @@ let rec cloop ~st pa =
     let st = execute ~st stm in
     (cloop [@ocaml.tailcall]) ~st pa
 
+let touch_file f =
+  let out = open_out f in
+  output_char out ' ';
+  close_out out
+
 let save_library ldir in_file =
   let out_vo = Filename.(remove_extension in_file) ^ ".vo" in
   let todo_proofs = Library.ProofsTodoNone in
-  Library.save_library_to todo_proofs ~output_native_objects:false ldir out_vo
+  Library.save_library_to todo_proofs ~output_native_objects:false ldir out_vo;
 
-let compile ~vo_path ~ml_path ~in_file =
+  let out_aux =
+    let d, f = Filename.dirname in_file, Filename.basename in_file in
+    let f = Filename.(remove_extension f) in
+    d ^ Filename.dir_sep ^ "." ^ f ^ ".aux"
+  in
+  let out_glob = Filename.(remove_extension in_file) ^ ".glob" in
+  touch_file out_aux;
+  touch_file out_glob
+
+let compile ~vo_path ~ml_path ~require_libs ~in_file =
   let f_in = open_in in_file in
-  let st, ldir = init_coq ~vo_path ~ml_path in_file in
+  let st, ldir = init_coq ~vo_path ~ml_path ~require_libs in_file in
   let pa = Pcoq.Parsable.make (Stream.of_channel f_in) in
-  let st = cloop ~st pa in
+  let _st = cloop ~st pa in
   (* Compact the heap, just in case. *)
   Gc.compact ();
-  print_st_stats st;
+  (* print_st_stats st; *)
   let () = save_library ldir in_file in
   ()
 
-let rec parse_args (args : string list) vo_acc ml_acc : _ * _ * string =
+let rec parse_args (args : string list) vo_acc ml_acc init boot coqlib
+  : _ * _ * _ * _ * _ * string =
   match args with
   | [] -> CErrors.user_err (Pp.str "parse args error: missing argument")
+  | "-noinit" :: rem ->
+    parse_args rem vo_acc ml_acc false boot coqlib
+  | "-boot" :: rem ->
+    parse_args rem vo_acc ml_acc init true coqlib
+  | "-q" :: rem ->
+    parse_args rem vo_acc ml_acc init boot coqlib
   | (("-Q" | "-R") as impl_str) :: d :: p :: rem ->
     let implicit = String.equal impl_str "-R" in
     let vo_acc = mk_vo_path d p implicit :: vo_acc in
-    parse_args rem vo_acc ml_acc
+    parse_args rem vo_acc ml_acc init boot coqlib
+  | "-coqlib" :: lib :: rem ->
+    parse_args rem vo_acc ml_acc init boot (Some lib)
+  | "-w" :: _ :: rem ->
+    parse_args rem vo_acc ml_acc init boot coqlib
+  | "-native-compiler" :: _ :: rem ->
+    parse_args rem vo_acc ml_acc init boot coqlib
   | "-I" :: d :: rem ->
     let ml_acc = d :: ml_acc in
-    parse_args rem vo_acc ml_acc
+    parse_args rem vo_acc ml_acc init boot coqlib
   | [file] ->
-    List.rev vo_acc, List.rev ml_acc, file
+    List.rev vo_acc, List.rev ml_acc, init, boot, coqlib, file
   | args ->
     let args_msg = String.concat " " args in
     CErrors.user_err Pp.(str "parse args error, too many arguments: " ++ str args_msg)
 
 let () =
+  Flags.quiet := true;
+  System.trust_file_cache := true;
   try
-    let vo_path, ml_path, in_file =
-      parse_args (List.tl @@ Array.to_list Sys.argv) default_vo_load_path [] in
-    compile ~vo_path ~ml_path ~in_file
+    let vo_path, ml_path, init, boot, coqlib, in_file =
+      parse_args (List.tl @@ Array.to_list Sys.argv) [] [] true false None in
+    let require_libs = if init then dft_require_libs else [] in
+    let vo_path = if boot then vo_path else default_vo_load_path ~coqlib @ vo_path in
+    compile ~vo_path ~ml_path ~require_libs ~in_file
   with exn ->
     Format.eprintf "Error: @[%a@]@\n%!" Pp.pp_with (CErrors.print exn)
