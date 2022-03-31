f59c6e5f192851c955627603b0ffa3bf12fcae56
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-03-31 18:32:16 +0200

    preliminary goal printing

--- compiler/coqJson.ml
+++ compiler/coqJson.ml
@@ -6,10 +6,20 @@ type t =
   ; parent_hash : string list
   } [@@deriving yojson]
 
-let make _ =
+let default =
   { proof_label = ""
   ; goal = ""
   ; proof_step = ""
   ; proof_step_hash = ""
   ; parent_hash = []
   }
+
+let make (pf : Proof.t) =
+  let { Proof.goals; sigma; _ } = Proof.data pf in
+  match goals with
+  | [gl] ->
+    let ei = Evd.find sigma gl in
+    let goal = Printer.pr_evar sigma (gl,ei) |> Pp.string_of_ppcmds in
+    { default with goal }
+  | _ ->
+    default
